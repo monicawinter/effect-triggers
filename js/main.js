88aaa35cc2534c06b2e50c7b7566f382fa726995
monicawinter <m.m.winter@hotmail.com>
2016-03-29 16:46:00 -0400

    added animation in to diamond

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 var $clicked = $('.btn-show-hide');
+var $move = $('.btn-move');
 
 $clicked.on('click', function (e) {
   $('.box').toggleClass('box-show');
+});
+
+$move.on('click', function (e) {
+  $('.diamond').toggleClass('diamond-move');
 });
